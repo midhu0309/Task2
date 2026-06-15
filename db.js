61d608fd47d62816dfadd417d47c654e8d2196b6
midhu0309 <145743709+midhu0309@users.noreply.github.com>
2026-06-15 13:08:51 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"taskmanager"
+});
+
+
+connection.connect((err)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("MySQL Connected");
+    }
+});
+
+
+module.exports = connection;
